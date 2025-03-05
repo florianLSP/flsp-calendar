de04b98504b9afc39681eedb5190af555fae2969
florian <florian.lesp@gmail.com>
2025-03-05 20:53:34 +0100

    feat: display month and year

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const currentDate = ref(new Date())
+const currentMonth = ref(
+  new Intl.DateTimeFormat('fr-FR', { month: 'long' }).format(currentDate.value),
+)
+const currentYear = ref(
+  new Intl.DateTimeFormat('fr-FR', { year: 'numeric' }).format(currentDate.value),
+)
+
+const months = ref([
+  'Janvier',
+  'Février',
+  'Mars',
+  'Avril',
+  'Mai',
+  'Juin',
+  'Juillet',
+  'Août',
+  'Septembre',
+  'Octobre',
+  'Novembre',
+  'Décembre',
+])
+</script>
 
 <template>
-  <div>Hello</div>
+  <div class="p-10">
+    <p class="capitalize">{{ currentMonth }} {{ currentYear }}</p>
+  </div>
 </template>
