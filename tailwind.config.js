90428b582ec1b019fda10bb8a26d592b0e01ab87
florian <florian.lesp@gmail.com>
2025-03-05 20:36:27 +0100

    feat: add tailwindcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        sen: {
+          light_red: '#A63D40',
+          dark_red: '#852C2F',
+          light_green: '#74A57F',
+          dark_green: '#4B6B51',
+          gray: '#536271',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
