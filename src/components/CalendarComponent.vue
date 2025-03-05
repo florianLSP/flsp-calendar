b07eb13befafa3e6c0f93286e2529d3b783863ea
florian <florian.lesp@gmail.com>
2025-03-05 21:03:49 +0100

    feat: add chevron next to the date

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/24/solid'
 
 const currentDate = ref(new Date())
 const currentMonth = ref(
@@ -26,7 +27,13 @@ const months = ref([
 </script>
 
 <template>
-  <div class="p-10">
+  <div class="p-10 flex items-center space-x-5">
+    <div class="flex space-x-1">
+      <ChevronLeftIcon
+        class="h-5 w-5 rounded-md hover:bg-gray-200 cursor-pointer transition items-center"
+      />
+      <ChevronRightIcon class="h-5 w-5 rounded-md hover:bg-gray-200 cursor-pointer transition" />
+    </div>
     <p class="capitalize">{{ currentMonth }} {{ currentYear }}</p>
   </div>
 </template>
