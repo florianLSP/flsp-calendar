5d8a664241959090c2f4231f53a9018d2550770f
florian <florian.lesp@gmail.com>
2025-03-10 14:39:50 +0100

    feat: create MonthNavigation component
    The main component was getting hard to follow, so I'm breaking it down into smaller components.

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/24/solid'
 import { useCalendarStore } from '@/stores/calendar'
+import MonthNavigation from './MonthNavigation.vue'
 
 const calendarStore = useCalendarStore()
-
-function updateMonth(step: number) {
-  if (calendarStore.selectedMonth == 0 && step == -1) {
-    calendarStore.selectedYear = parseInt(calendarStore.currentYear) - 1
-  } else if (calendarStore.selectedMonth == 11 && step == 1) {
-    calendarStore.selectedYear = parseInt(calendarStore.currentYear) + 1
-  }
-
-  calendarStore.selectedMonth =
-    (calendarStore.selectedMonth + step + calendarStore.months.length) % calendarStore.months.length
-}
 
 function getDaysInMonth(year: number, month: number): number {
   return new Date(year, month + 1, 0).getDate()
@@ -38,16 +27,7 @@ function getFirstDayOfMonth(year: number, month: number) {
       >
         Aujourd'hui
       </button>
-      <div class="flex space-x-1">
-        <ChevronLeftIcon
-          @click="updateMonth(-1)"
-          class="h-5 w-5 rounded-md hover:bg-gray-200 cursor-pointer transition items-center"
-        />
-        <ChevronRightIcon
-          @click="updateMonth(1)"
-          class="h-5 w-5 rounded-md hover:bg-gray-200 cursor-pointer transition"
-        />
-      </div>
+      <MonthNavigation />
       <p class="capitalize text-xl">
         <span>{{ calendarStore.months[calendarStore.selectedMonth] }}</span>
         {{ calendarStore.currentYear }}
