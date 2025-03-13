9c775a0148b3918ab72ce89c4c5fbfb264101ada
florian <florian.lesp@gmail.com>
2025-03-13 10:10:32 +0100

    fix: prevent event name overflow in calendar cells

--- src/components/NewEvent.vue
+++ src/components/NewEvent.vue
@@ -2,6 +2,8 @@
 
 <template>
   <div class="bg-blue-500 w-full rounded-md">
-    <p class="pl-2 text-white">Nouvel événement</p>
+    <p class="pl-2 text-white text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+      Nouvel événement
+    </p>
   </div>
 </template>
