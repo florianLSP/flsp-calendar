0b725d048478765655c5c87abc90defea633f64f
Florian <florian.lesp@gmail.com>
2025-03-21 12:31:48 +0100

    feat: creat new modal to display event details

---
+++ src/components/header/EventDetailsModal.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/solid'
+
+const isOpen = ref(true)
+
+function closeModal() {
+  isOpen.value = false
+}
+
+const props = defineProps({
+  eventTitle: String,
+  eventDescription: String,
+  eventDay: Number,
+  eventMonth: Number,
+  eventYear: Number,
+})
+</script>
+<template>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25 backdrop-blur-sm"></div>
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <XMarkIcon
+                class="absolute top-4 right-4 rounded-md hover:bg-gray-200 cursor-pointer transition h-5 w-5"
+                @click="closeModal"
+              />
+              <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
+                {{ props.eventTitle }}
+              </DialogTitle>
+              <div class="mt-4 w-full space-y-4">
+                <p>description: {{ props.eventDescription }}</p>
+                <p>date: {{ props.eventDay }}/{{ props.eventMonth }}/{{ props.eventYear }}</p>
+              </div>
+
+              <div class="mt-8 space-x-5 flex justify-end">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-flsp-light_red px-4 py-2 text-sm font-medium text-flsp-dark_red hover:bg-flsp-medium_red focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Annuler
+                </button>
+
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-flsp-light_blue px-4 py-2 text-sm font-medium text-flsp-dark_blue hover:bg-flsp-medium_blue focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                >
+                  Créer
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
