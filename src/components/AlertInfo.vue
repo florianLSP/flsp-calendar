dbd0abc993d3038596e197df9a04274e1a98e97a
Florian <florian.lesp@gmail.com>
2025-03-26 09:27:35 +0100

    feat: create AlertInfo component
    This component handles alert that pop up when a user tries to validate the form without providing a title for the event.

---
+++ src/components/AlertInfo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { XMarkIcon } from '@heroicons/vue/24/solid'
+</script>
+<template>
+  <div
+    id="alert-2"
+    class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+    role="alert"
+  >
+    <svg
+      class="shrink-0 w-4 h-4"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+      />
+    </svg>
+    <div class="ms-3 text-sm font-medium">L'événement doit avoir un titre !</div>
+    <button
+      type="button"
+      class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+      data-dismiss-target="#alert-2"
+      aria-label="Close"
+    >
+      <XMarkIcon class="h-5 w-5" />
+    </button>
+  </div>
+</template>
