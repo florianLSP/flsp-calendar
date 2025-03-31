efcd0fb6b4510c0b40bef4073a5d03bb6f19e038
Florian <florian.lesp@gmail.com>
2025-03-31 15:25:26 +0200

    style: replace SVG with Heroicons icon

--- src/components/AlertInfo.vue
+++ src/components/AlertInfo.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { XMarkIcon } from '@heroicons/vue/24/solid'
+import { InformationCircleIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps({
   alertMessage: String,
@@ -11,17 +12,7 @@ const props = defineProps({
     class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
     role="alert"
   >
-    <svg
-      class="shrink-0 w-4 h-4"
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-    >
-      <path
-        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
-      />
-    </svg>
+    <InformationCircleIcon class="h-5 w-5" />
     <div class="ms-3 text-sm font-medium">{{ props.alertMessage }}</div>
     <button
       type="button"
